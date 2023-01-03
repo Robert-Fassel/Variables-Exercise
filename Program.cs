d6dadbfc8e72821efa8fcba51879781c3ab0a03a
Robert Fassel <fcwrob@gmail.com>
2023-01-02 21:05:26 -0600

    Variable Exercise init

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Declare and initialize variables
+string name = "Rob";
+int age = 37;
+char middleInitial = 'E';
+bool isOver18 = true;
+double currentTemp = 53.9;
+decimal currentPrice = 2.70m;
+
+Console.WriteLine($"My name is {name} {middleInitial}.");
+Console.WriteLine($"At the time or writing this I am {age} years old.");
+Console.WriteLine($"That makes me over 18, {isOver18} enough.");
+Console.WriteLine($"{currentTemp} degrees is the current temperature.");
+Console.WriteLine($"Gas is ${currentPrice} a gallon.");
